6bd776497e46e7793b9218a3f534503eea41aca3
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-04-06 20:05:21 +0530

    feat[navbar]: integrate Navbar component into app layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, ReactiveFormsModule, CommonModule],
+  imports: [RouterOutlet, RouterModule, ReactiveFormsModule, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
